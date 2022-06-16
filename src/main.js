d65be97ac29f6cfb9e3f6244aea20182025ed3e2
fffqwhy <599769224@qq.com>
2022-06-17 00:17:06 +0800

    样式资源使用webpack打包，包括scss,sass,less,css,stylus

--- src/main.js
+++ src/main.js
@@ -1,5 +1,11 @@
 import {sum} from './common/sum';
 import {desum} from './common/desum';
+import './css/index.css';
+import './less/index.less';
+import './sass/index.sass';
+import './scss/index.scss';
+import './stylus/index.styl';
+
 
 console.log(sum(1,4));
 console.log(desum(2,1));
--- webpack.config.js
+++ webpack.config.js
@@ -11,11 +11,26 @@ module.exports = {
         filename: "build.js"
     },
     // 加载器
-    // module: {
-    //     rules: {
-    //         // loader 的配置
-    //     }
-    // },
+    module: {
+        rules: [
+            {
+                test:/\.css$/,
+                use:["style-loader","css-loader"]
+            },
+            {
+                test: /\.less$/,
+                use:["style-loader","css-loader","less-loader"]
+            },
+            {
+                test: /\.s[ac]ss$/,
+                use:["style-loader","css-loader","sass-loader"]
+            },{
+                test: /\.styl$/,
+                use:["style-loader","css-loader","stylus-loader"]
+            }
+        ]
+            // loader 的配置
+    },
     //插件
     // plugins: [
 
