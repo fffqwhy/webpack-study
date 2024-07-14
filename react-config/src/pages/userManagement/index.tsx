3e3de93116c01842fb96155b98fff105b5ed6c63
qyfu <599769224@qq.com>
2024-07-14 23:45:34 +0800

    优化路由。。

---
+++ react-config/src/pages/userManagement/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { FunctionComponent } from "react";
+
+interface UserManagementProps {
+
+}
+
+const UserManagement: FunctionComponent<UserManagementProps> = () => {
+    return (<div>用户管理</div>);
+}
+
+export default UserManagement;
--- react-config/src/route/index.tsx
+++ react-config/src/route/index.tsx
@@ -3,30 +3,35 @@ import { Navigate, createHashRouter } from "react-router-dom";
 import Todo from "../pages/home/todo";
 import TestTable from "../pages/home/table";
 import Home from "../pages/home";
+import UserManagement from "../pages/userManagement";
 
 const router = createHashRouter([
-  {
-    path: "home",
-    element: <Home />,
-    children: [
-      {
-        path: "todo",
-        element: <Todo />,
-      },
-      {
-        path: "onePage",
-        element: <TestTable />,
-      },
-      {
+    {
+        path: "home",
+        element: <Home />,
+        children: [
+            {
+                path: "todo",
+                element: <Todo />,
+            },
+            {
+                path: "onePage",
+                element: <TestTable />,
+            },
+            {
+                path: "",
+                element: <Navigate to="todo" />,
+            },
+        ],
+    },
+    {
+        path: "user",
+        element: <UserManagement />
+    },
+    {
         path: "",
-        element: <Navigate to="todo" />,
-      },
-    ],
-  },
-  {
-    path: "",
-    element: <Navigate to="home" />,
-  },
+        element: <Navigate to="home" />,
+    },
 ]);
 
 export default router;
