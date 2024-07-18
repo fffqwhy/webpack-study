409391628838ce200a3e891d07ef79bbd850e192
qyfu <599769224@qq.com>
2024-07-19 00:15:41 +0800

    修改 dispatch - redux 中的值

--- react-config/src/pages/userManagement/index.tsx
+++ react-config/src/pages/userManagement/index.tsx
@@ -1,16 +1,25 @@
 import React from "react";
 import { FunctionComponent } from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { storeT } from "../../redux/store";
+import { Button } from "antd";
+import { setUserInfo } from "../../redux/action/user";
 
 interface UserManagementProps {
 
 }
 
 const UserManagement: FunctionComponent<UserManagementProps> = () => {
-    const {userName} = useSelector<storeT,storeT["userInfoReducer"]>((state)=>state.userInfoReducer);
+    const dispatch = useDispatch();
+    const { userName, loginTime } = useSelector<storeT, storeT["userInfoReducer"]>((state) => state.userInfoReducer);
+    const changeUserInfo = () => {
+        dispatch(setUserInfo({ userName: "qyfu"+new Date().getSeconds(), loginTime: new Date() }))
+    }
     return (<div>
-        用户管理{userName}
+        <div>
+            用户管理{userName}
+        </div>
+        <Button type="primary" onClick={changeUserInfo}>修改redux userinfo {(loginTime as Date).getMilliseconds()}</Button>
     </div>);
 }
 
--- react-config/src/redux/action/user/index.ts
+++ react-config/src/redux/action/user/index.ts
@@ -3,6 +3,6 @@ import { UPDATE_USER_INFO } from "./actionType"
 export const setUserInfo = (data:any)=>{
     return {
         type:UPDATE_USER_INFO,
-        text:data
+        data:data
     }
 }
