6b80e5ca98aa7304b3c9a359dd91be8bd2cf0000
qyfu <599769224@qq.com>
2024-07-22 23:59:26 +0800

    使用redux-thunk 进行异步操作

--- react-config/src/pages/userManagement/index.tsx
+++ react-config/src/pages/userManagement/index.tsx
@@ -2,8 +2,8 @@ import React from "react";
 import { FunctionComponent } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { storeT } from "../../redux/store";
-import { Button } from "antd";
-import { setUserInfo } from "../../redux/action/user";
+import { Button, Space } from "antd";
+import { fetchData, setUserInfo } from "../../redux/action/user";
 
 interface UserManagementProps {
 
@@ -13,13 +13,19 @@ const UserManagement: FunctionComponent<UserManagementProps> = () => {
     const dispatch = useDispatch();
     const { userName, loginTime } = useSelector<storeT, storeT["userInfoReducer"]>((state) => state.userInfoReducer);
     const changeUserInfo = () => {
-        dispatch(setUserInfo({ userName: "qyfu"+new Date().getSeconds(), loginTime: new Date() }))
+        dispatch(setUserInfo({ userName: "qyfu" + new Date().getSeconds(), loginTime: new Date() }))
+    }
+    const changeUserInfoBythunk = () => {
+        dispatch(fetchData() as any)
     }
     return (<div>
         <div>
             用户管理{userName}
         </div>
-        <Button type="primary" onClick={changeUserInfo}>修改redux userinfo {(loginTime as Date).getMilliseconds()}</Button>
+        <Space>
+            <Button type="primary" onClick={changeUserInfo}>修改redux userinfo {(loginTime as Date).getMilliseconds()}</Button>
+            <Button type="primary" onClick={changeUserInfoBythunk}>修改redux-thunk {(loginTime as Date).getMilliseconds()}</Button>
+        </Space>
     </div>);
 }
 
--- react-config/src/redux/action/user/index.ts
+++ react-config/src/redux/action/user/index.ts
@@ -1,8 +1,33 @@
-import { UPDATE_USER_INFO } from "./actionType"
+import { UPDATE_USER_INFO } from "./actionType";
 
-export const setUserInfo = (data:any)=>{
-    return {
-        type:UPDATE_USER_INFO,
-        data:data
+export const setUserInfo = (data: any) => {
+  return {
+    type: UPDATE_USER_INFO,
+    data: data,
+  };
+};
+
+// 异步action，使用redux-thunk
+export const fetchData = () => {
+  return async (dispatch: any) => {
+    try {
+      const res = await new Promise((reslove) => {
+        setTimeout(() => {
+          reslove(true);
+        }, 1000);
+      });
+      if (res === true) {
+        dispatch(
+          setUserInfo({
+            userName: "thunkqyfu" + new Date().getSeconds(),
+            loginTime: new Date(),
+          })
+        );
+      }
+    } catch (error) {
+      console.error("Error fetching data:", error);
+      // 你可以在这里分发一个失败的 action，比如
+      // dispatch({ type: 'FETCH_DATA_FAILURE', error });
     }
-}
+  };
+};
